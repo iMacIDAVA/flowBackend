c989c8c1f35eeedf0ec157177a637796af8c6e16
iMacIDAVA <imacidava@gmail.com>
2025-08-01 16:06:17 +0300

    fix no csss bug in the deployment

--- consultation_platform/settings.py
+++ consultation_platform/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
